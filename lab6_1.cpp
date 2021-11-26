14b014bf609dbf03722ab2b3e4810ba30d41c927
tanawat640610678 <thanawat_chai@cmu.ac.th>
2021-11-26 20:07:26 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int N;
+    int i = 0;
+    int j = 0;
+    while(N != 0){
+        cout << "Enter an integer: ";
+        cin >> N;
+        
+        if(N%2 ==0){
+            i = i+1;
+            if(N == 0){
+                i = i-1; }
+        } else{
+            j = j+1;}
+         }
+    cout << "#Even numbers = " << i ;
+    cout << "\n#Odd numbers = " << j ;
     return 0;
 }
+
